5f46b56950f96404ab1c60cd77b26b173f18ce26
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-05-24 17:54:21 -0400

    Added Navigation View Model, MenuItems

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using ArtHive.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtHive.Components.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+       public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<MenuItem>
+            {
+                new MenuItem { Controller = "Home", Action = "Index", Label = "Home" },
+                new MenuItem { Controller = "Home", Action = "About", Label = "About" },
+                new MenuItem { Controller = "Categories", Action = "Index", Label = "Categories", DropdownItems = new List<MenuItem>{
+                    new MenuItem {Controller = "Categories", Action = "Index", Label = "List"},
+                    new MenuItem {Controller = "Categories", Action = "Create", Label = "Create"},
+                } },
+                new MenuItem { Controller = "Home", Action = "Privacy", Label = "Privacy" },
+            };
+            return View(menuItems);
+        } 
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtHive.Models
+{
+    public class MenuItem
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Label { get; set; }
+
+        public List<MenuItem> DropdownItems { get; set; }
+    }
+}
