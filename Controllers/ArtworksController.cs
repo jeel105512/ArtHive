9571e1e57bc9d7aab210d20a68cda2c058530743
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-07-11 17:52:59 -0400

    Delete functionality for Files added.

--- Controllers/ArtworksController.cs
+++ Controllers/ArtworksController.cs
@@ -113,11 +113,58 @@ namespace ArtHive.Controllers
                 return NotFound();
             }
 
+            // Using the FindAsync method instead of FirstOrDefault to retrieve the existing book.
+            var existingArtWork = await _context.Artworks.FindAsync(id);
+
+            if (existingArtWork == null)
+            {
+                return NotFound();
+            }
+
             if (ModelState.IsValid)
             {
+                if (Image != null && Image.Length > 0) // delete the existing file and update it with new one if a file already exists
+                {
+                    var existingImageLocation = System.IO.Directory.GetCurrentDirectory() + "\\wwwroot\\images\\artworks\\" + existingArtWork.Image;
+                    // delete the existing image
+                    FileInfo existingImageFileInfo = new FileInfo(existingImageLocation);
+                    if (existingImageFileInfo.Exists)
+                    {
+                        existingImageFileInfo.Delete();
+                    }
+
+                    byte[] fileBytes;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await Image.CopyToAsync(memoryStream);
+                        fileBytes = memoryStream.ToArray();
+                    }
+
+                    if (!IsJpegFile(fileBytes) && !IsPngFile(fileBytes))
+                    {
+                        ModelState.AddModelError("Image", "Only .jpeg or .png files are allowed");
+                        ViewData["CollectionId"] = new SelectList(_context.Collections, "Id", "Name", artwork.CollectionId);
+                        return View(artwork);
+                    }
+                    // Save the new image and assign the file name to artwork.Image
+                    artwork.Image = await UploadPhoto(Image);
+                } else if(existingArtWork.Image != null) // upload the file if the new file is uploaded and there is no existing file
+                {
+                    // keep the existing photo if no new one uploaded
+                    artwork.Image = existingArtWork.Image;
+                }
                 try
                 {
-                    artwork.Image = await UploadPhoto(Image);
+                    // Deatch existingArtWork from the context
+                    _context.Entry(existingArtWork).State = EntityState.Detached;
+                    
+                    existingArtWork.CollectionId = artwork.CollectionId;
+                    existingArtWork.Title = artwork.Title;
+                    existingArtWork.Creator = artwork.Creator;
+                    existingArtWork.Description = artwork.Description;
+                    existingArtWork.Price = artwork.Price;
+
+                    // artwork.Image = await UploadPhoto(Image);
 
                     _context.Update(artwork);
                     await _context.SaveChangesAsync();
@@ -168,11 +215,22 @@ namespace ArtHive.Controllers
                 return Problem("Entity set 'ApplicationDbContext.Artworks'  is null.");
             }
             var artwork = await _context.Artworks.FindAsync(id);
-            if (artwork != null)
+            if (artwork == null)
             {
-                _context.Artworks.Remove(artwork);
+                return NotFound();
             }
-            
+
+            // Delete the associating image file
+            if (!string.IsNullOrEmpty(artwork.Image))
+            {
+                string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", "artworks", artwork.Image);
+                if(System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+
+            _context.Artworks.Remove(artwork);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
@@ -180,6 +238,25 @@ namespace ArtHive.Controllers
         private bool ArtworkExists(int id)
         {
           return (_context.Artworks?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        // Helper method to check if the file is a JPEG (JFIF) or if the file is a PNG
+        private bool IsJpegFile(byte[] fileBytes)
+        {
+            return fileBytes.Length > 2 && fileBytes[0] == 0xFF && fileBytes[1] == 0xD8;
+        }
+
+        private bool IsPngFile(byte[] fileBytes)
+        {
+            return fileBytes.Length > 8 &&
+                fileBytes[0] == 0x89 &&
+                fileBytes[1] == 0x50 &&
+                fileBytes[2] == 0x4E &&
+                fileBytes[3] == 0x47 &&
+                fileBytes[4] == 0x0D &&
+                fileBytes[5] == 0x0A &&
+                fileBytes[6] == 0x1A &&
+                fileBytes[7] == 0x0A;
         }
     }
 }
