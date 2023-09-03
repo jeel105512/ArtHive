210d8a57e2e832af8e783fd5c280cd45a40a016b
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-09-03 09:35:43 -0400

    First Test

---
+++ Controllers/UnitTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtHive.Controllers
+{
+    public class UnitTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
