810c4970e29e855b9c726cad82ba3a74e406f2ec
JeelKumar Patel <jeelhpatel10@gmail.com>
2023-09-04 12:17:30 -0400

    documentation

--- Program.cs
+++ Program.cs
@@ -40,6 +40,10 @@ namespace ArtHive
                 options.Cookie.IsEssential = true;
             });
 
+            // swashbuckle for api docs
+            // documentation: https://learn.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-7.0&tabs=visual-studio
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             app.UseSession();
@@ -48,6 +52,9 @@ namespace ArtHive
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
+                // documentation: https://learn.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-7.0&tabs=visual-studio
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             else
             {
@@ -55,6 +62,16 @@ namespace ArtHive
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // when the app starts, use Swagger to input any API controllers and generate new documentation file
+            // documentation: https://learn.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-7.0&tabs=visual-studio
+            // for json web tokens: https://learn.microsoft.com/en-us/aspnet/core/security/authentication/jwt-authn?view=aspnetcore-7.0&tabs=windows
+            //app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swager.json", "ArtHive API");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
